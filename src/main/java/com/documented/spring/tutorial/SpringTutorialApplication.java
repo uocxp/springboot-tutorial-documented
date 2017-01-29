29bcf9479a6f308901ade4fa289607a500c30a73
uocxp <ali-akil@gmx.de>
2017-01-29 19:22:54 +0100

    little changes

--- src/main/java/com/documented/spring/tutorial/SpringTutorialApplication.java
+++ src/main/java/com/documented/spring/tutorial/SpringTutorialApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NpSpringTutorialApplication {
+public class SpringTutorialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NpSpringTutorialApplication.class, args);
+		SpringApplication.run(SpringTutorialApplication.class, args);
 	}
 }
--- src/test/java/com/documented/spring/tutorial/SpringTutorialApplicationTests.java
+++ src/test/java/com/documented/spring/tutorial/SpringTutorialApplicationTests.java
@@ -4,15 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.documented.spring.tutorial.NpSpringTutorialApplication;
+import com.documented.spring.tutorial.SpringTutorialApplication;
 
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = NpSpringTutorialApplication.class)
+@SpringApplicationConfiguration(classes = SpringTutorialApplication.class)
 @WebAppConfiguration
-public class NpSpringTutorialApplicationTests {
+public class SpringTutorialApplicationTests {
 
 	@Test
 	public void contextLoads() {
